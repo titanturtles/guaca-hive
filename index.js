3824cc4eec6afbe73215fb7c6c82da6c6c093894
Titan Turtles <titanturtlesteam@gmail.com>
2023-06-21 23:57:44 +0000

    init response

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express')
-const body_parser = require('body-parser')
 const app = express()
+
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
 
 app.get('/', function (req, res) {
     console.log('req.params', req.params);
     res.send('received')
 })
 
-app.post('/', body_parser, (req, res) => {
+app.post('/', (req, res) => {
     console.log('req.body', req.body)
     res.send('received')
 })
 
+app.post('/api/health', (req, res) => {
+    console.log(new Date().toISOString(), '/api/health', req.body)
+    res.send('/api/health received')
+})
+
+app.post('*', (req, res) => {
+    console.log('*', req.body)
+    res.send('* received')
+})
+
 app.listen(20002)
+console.log('listening to port 20002')
